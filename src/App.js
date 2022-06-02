d12fb936bff0b2a3157697295a0e15f24ccb35b2
haru <haruki6180@gmail.com>
2022-06-02 16:24:23 +0900

    データ取得

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { db } from "./firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+  const usersColelctionRef = collection(db, "users");
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersColelctionRef);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getUsers();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users.map((user) => {
+        return (
+          <div>
+            <h1>Name: {user.name}</h1>
+            <h1>Age: {user.age}</h1>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
